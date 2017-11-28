e243f37f9b8dc393341fd5eb8fda0c4922cb20a2
Callum <32675933+seeley-c@users.noreply.github.com>
2017-11-28 17:16:12 -0600

    Add files via upload

--- Assignment 1/src/A1Q1.java
+++ Assignment 1/src/A1Q1.java
@@ -5,11 +5,13 @@
  * Assignment: Assignment 1 Question 1
  * Program Name:  A1Q1.java
  *
- * Description: 
+ * Description: Generates numbers between 1-100 for an array that is
+ *              1000 numbers long, and outputs the array to a text file
+ *              five numbers per line
  *
- * Input: 
+ * Input:       none
  *
- * Output: 
+ * Output:      1000 numbers between 1-100 to a text file, 5 per line
  ***********************************************************************/
  
  // import java libraries here as needed
@@ -26,24 +28,26 @@ public class A1Q1 {  // begin class
     
     // ********* declaration of constants **********
         
-        final int MAXLIST = 1000;
-        final int MAX = 100;
+        final int MAXLIST = 1000;       //max length of array
+        final int MAX = 100;            //max value for generated number
         
     // ********** declaration of variables **********
         
-        int array[] = new int[MAXLIST];
-        int counter = 0;
+        int array[] = new int[MAXLIST]; 
+        //array that will hold the generated values
+        int counter = 0; //counter for outputting 5 numbers per line
     	
     // create instances of objects for i/o and formatting
     
     	
-    	PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("textOut.txt")));
+    	PrintWriter fout = new PrintWriter(new BufferedWriter
+            (new FileWriter("textOut.txt")));
         ProgramInfo programinfo = new ProgramInfo("A1 " + "Q1");
-        Random rand = new Random();
+        Random rand = new Random(); //random value generator
     	
     // ********** Print output Banner **********
     
-    	System.out.println(programinfo.toString());
+    	System.out.println(programinfo.toString()); //output program info
         //fout.println(programinfo.toString());
  	    	
     // ************************ get input **********************
@@ -52,7 +56,7 @@ public class A1Q1 {  // begin class
 
     // ************************ processing ***************************
     
-        for (int n = 0; n < MAXLIST; n++) {
+        for (int n = 0; n < MAXLIST; n++) { //loop for generating values
              array[n] = rand.nextInt (MAX)+ 1; //generate random ints
              
         } //end for loop for generating numbers
@@ -62,15 +66,15 @@ public class A1Q1 {  // begin class
         //fileList(array, MAXLIST, fout);
         //printList(array, MAXLIST, fout);
         
-        for (int n = 0; n <MAXLIST; n++) {
-            fout.print(array[n] + " ");
-            counter++;
+        for (int n = 0; n <MAXLIST; n++) {  //output for loop
+            fout.print(array[n] + " "); //output number to line
+            counter++;  //add one to counter, for outputting 5 per line
             
-            if (counter == 5) {
-                fout.println("");
-                counter = 0;
-            }
-        }
+            if (counter == 5) { //if statement for outputting 5 per line
+                fout.println("");   //create new line
+                counter = 0; //reset counter
+            }//end if statement
+        }//end output for loop
         
     
     // ******** closing message *********
--- Assignment 1/src/A1Q2.java
+++ Assignment 1/src/A1Q2.java
@@ -27,13 +27,29 @@ public class A1Q2 {  // begin class
     // ********* declaration of constants **********
         
         final int MAXLIST = 1000;
-        final int MAX = 100;
-        final int MIN = 0;
+        final int MAX = 101;
+        final int SenVal = -1;
+        final String stringSenVal = "-1";
         
     // ********** declaration of variables **********
         
-        String delim = " ";
+        String delim = "[ ]+";
         int array[] = new int[MAXLIST];
+        String strin = "";
+        String input1[] = null;
+        int count = 0;
+        int num = 0;
+        int freq[] = new int[MAX];
+        
+        String delim2 = "[-]+";
+        int inLeng = 0;
+        int inSingleNum = 0;
+        String inMultNum = "";
+        String tokens[] = null;
+        int n1 = 0;
+        int n2 = 0;
+        
+        
     	
     // create instances of objects for i/o and formatting
     
@@ -47,15 +63,99 @@ public class A1Q2 {  // begin class
         fout.println(programinfo.toString());
  	    	
     // ************************ get input **********************
-
-    	for (int n = 0; n < MAXLIST; n++) {
+    
+        strin = fin.readLine();
+        count = 0;
+        
+    	while (strin != null) {
+            input1 = strin.split(delim);
+            System.out.println(strin + "\n");
             
-        }
-
+            for (int n = 0; n< 5; n++) {
+                array[count] = Integer.parseInt(input1[n]);
+                //System.out.println("Number " + count + " is generated " + array[count] + " times\n");
+                
+                count++;
+            }//end for loop
+            
+            strin = fin.readLine();
+        }//end while loop
+        
+        
     // ************************ processing ***************************
     
         
     
+        for (num = 1; num < MAX ; num++) {
+        for (int n = 0; n < MAXLIST; n++) {
+            
+            
+            if (array[n] == num) {
+                freq[num]++;
+                
+                //System.out.println(freq[num]);
+            }//end if statement
+            //System.out.println(freq[num]);
+            
+            
+        }//end for loop
+        //System.out.println(freq[num]);
+        System.out.println("Number " + num + " is generated " + freq[num] + " times\n");
+        }//end for loop
+        
+        
+        
+        
+        while (!(inSingleNum == SenVal || inMultNum.equals(stringSenVal))) {
+            
+            try {
+            inSingleNum = Integer.parseInt(JOptionPane.showInputDialog(
+            "Enter the number that you would like to see the frequency of "
+                    + " or -1 to end"));
+            
+            if (inSingleNum != SenVal) {
+                System.out.println("The frequency of " + inSingleNum + " is "  
+                    + freq[inSingleNum]);
+            }
+            }
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(
+                        "Please enter a single number, between 1-100");
+            }
+            catch (NumberFormatException i) {
+                System.out.println(
+                        "Please enter a single number, between 1-100");
+            }
+            
+            try {
+            inMultNum = JOptionPane.showInputDialog(
+            "Enter the numbers that you would like to see the frequency of "
+                    + "(ex: 10-20), or -1 to end");
+            
+            if (!(inMultNum.equals(stringSenVal))) {
+            tokens = inMultNum.split(delim2);
+            n1 = Integer.parseInt(tokens[0]);
+            n2 = Integer.parseInt(tokens[1]);
+            
+            for (int n = n1; n <= n2; n++) {
+                System.out.println("The frequency of " + n + " is "  
+                   + freq[n]);
+            }
+            }
+            }//end try
+            catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println(
+                        "Enter two numbers between 1-100, seperated by '-'");
+            }
+            catch (NumberFormatException i) {
+                System.out.println(
+                        "Enter two numbers between 1-100, seperated by '-'");
+            }
+            
+            
+        }//end while loop
+        
+        
     // ************************ print output ****************************
 
         //fileList(array, MAXLIST, fout);
@@ -68,7 +168,7 @@ public class A1Q2 {  // begin class
         
     // ***** close streams *****
         
-        //fin.close();			// close input buffer
+        fin.close();			// close input buffer
         fout.close();			// close output buffer
     }  // end main
     
